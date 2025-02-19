43c98c9d4f8dbf9ff14ea41b661d98869400d048
Maurício <66374666+MauricioOliveira30@users.noreply.github.com>
2025-02-19 19:33:08 -0300

    Update D002.py

--- D002.py
+++ D002.py
@@ -1,18 +1,16 @@
 # Função que calcula a sequência de Fibonacci até um determinado limite
-def fibonacci(limit):
-    seq = [0, 1]
-    while seq[-1] < limit:
-        seq.append(seq[-1] + seq[-2])
-    return seq
-
+def fibonacci():
+   
 # Pede um número ao usuário
 n= int(input("Digite um número inteiro: "))
 
 # Calcula a sequência de Fibonacci até o número digitado
-seq_fibonacci = fibonacci(n)
 
-# Verifica se o número pertence à sequência
-if n in seq_fibonacci:
-    print(f"{n} pertence à sequência de Fibonacci.")
-else:
-    print(f"{n} não pertence à sequência de Fibonacci.")
+t1=0
+t2=1
+for i in range(0,num):
+    fibo=t1+t2
+    t1=t2
+    t2=fibo
+    print(f"A sequência fibonacci é {fibo}")
+
